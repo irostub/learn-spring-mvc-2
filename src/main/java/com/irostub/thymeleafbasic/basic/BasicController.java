666243e41b7074e129648c676d1af90f1b7c7186
irostub <irostub@gmail.com>
2021-07-12 15:14:44 +0900

    Add using thymeleaf standard objects and ext objects

--- src/main/java/com/irostub/thymeleafbasic/basic/BasicController.java
+++ src/main/java/com/irostub/thymeleafbasic/basic/BasicController.java
@@ -1,9 +1,12 @@
 package com.irostub.thymeleafbasic.basic;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping(value = "basic")
@@ -19,5 +22,18 @@ public class BasicController {
     public String unescape(Model model) {
         model.addAttribute("data", "hello <b>spring!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping(value = "basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello "+data;
+        }
     }
 }
