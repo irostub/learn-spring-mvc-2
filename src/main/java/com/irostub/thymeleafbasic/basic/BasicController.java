49a20bd7c750e2e4fb84464db7e14cb772082f80
irostub <irostub@gmail.com>
2021-07-18 20:39:25 +0900

    Add using thymeleaf th:block tag and inline javascript syntax

--- src/main/java/com/irostub/thymeleafbasic/basic/BasicController.java
+++ src/main/java/com/irostub/thymeleafbasic/basic/BasicController.java
@@ -81,6 +81,20 @@ public class BasicController {
         model.addAttribute("data", "Spring!");
         return "basic/comments";
     }
+
+    @GetMapping(value = "block")
+    public String block(Model model) {
+        addUsers(model);
+        return "basic/block";
+    }
+
+    @GetMapping(value = "javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("UserA", 100));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     private void addUsers(Model model) {
         List<User> list = new ArrayList<>();
         list.add(new User("userA", 10));
