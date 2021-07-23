63d0931a52b98e73cac0a6247963c8e013be5b9e
irostub <irostub@gmail.com>
2021-07-23 17:30:05 +0900

    Update validation annotation in Item Object

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,26 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
 
     private Long id;
+
+    @NotBlank(message="공백은 넣을 수 없습니다.")
     private String itemName;
+
+    @NotNull
+    @Range(min=1_000, max = 1_000_000)
     private Integer price;
+
+    @NotNull
+    @Max(9999)
     private Integer quantity;
 
     public Item() {
