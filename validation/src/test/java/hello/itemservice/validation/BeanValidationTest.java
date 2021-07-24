1335ee1b592f0ab232c63d7d86e1b1147fb07518
irostub <irostub@gmail.com>
2021-07-24 16:06:05 +0900

    Add BeanValidationTest.java

---
+++ validation/src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,31 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+    @Test
+    @DisplayName("빈검증 기본 테스트")
+    void beanValidation(){
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
+        Item item = new Item();
+        item.setItemName("  ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> validations = validator.validate(item);
+        for (ConstraintViolation<Item> validation : validations) {
+            System.out.println("validation = " + validation);
+            System.out.println("validation.getMessage() = " + validation.getMessage());
+        }
+    }
+}
