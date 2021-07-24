097f245a5c18809e3a23a62a815e0707016128c6
irostub <irostub@gmail.com>
2021-07-24 16:20:22 +0900

    Add object validation, using @ScriptAssert annotation

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,16 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//오브젝트 검증을 위해서 다음과 같은 어노테이션을 사용한다.
+//다만 그 사용성이 복잡하기에 권장하지 않는 방법. 단순히 검증로직을 따로 빼내어서 작성하는 편이 더 나은 방법이라는 판단이 선다.
+//@ScriptAssert(lang="javascript", script="_this.price * _this.quantity >= 10000")
 public class Item {
 
     private Long id;
