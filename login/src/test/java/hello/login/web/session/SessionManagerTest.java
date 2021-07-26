48590bc97d1284854db21a1ac4634f16b75d0f02
irostub <irostub@gmail.com>
2021-07-26 18:12:13 +0900

    Implement session manager test

---
+++ login/src/test/java/hello/login/web/session/SessionManagerTest.java
@@ -0,0 +1,36 @@
+package hello.login.web.session;
+
+import hello.login.domain.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionManagerTest {
+
+    SessionManager sessionManager = new SessionManager();
+
+    @Test
+    @DisplayName("세션 생성, 만료 테스트")
+    void sessionTest() {
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        Member member = new Member();
+        sessionManager.createSession(member, response);
+
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setCookies(response.getCookies());
+
+        //세션 조회
+        Object result = sessionManager.getSession(request);
+        assertThat(result).isEqualTo(member);
+
+        //세션 만료
+        sessionManager.expire(request);
+        Object session = sessionManager.getSession(request);
+        assertThat(session).isNull();
+    }
+}
