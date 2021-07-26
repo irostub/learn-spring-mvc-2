3000de3ec6799dddf96603cc274e87b3425a4bef
irostub <irostub@gmail.com>
2021-07-26 19:22:33 +0900

    Update home controller
    use spring annotation @SessionAttribute instead of HttpSession

--- login/src/main/java/hello/login/web/HomeController.java
+++ login/src/main/java/hello/login/web/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -49,7 +50,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeV3(HttpServletRequest request, Model model) {
 
         HttpSession session = request.getSession(false);
@@ -58,6 +59,17 @@ public class HomeController {
         }
 
         Member member = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        if (member == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeV3Spring(@SessionAttribute(name=SessionConst.LOGIN_MEMBER, required = false) Member member, Model model) {
 
         if (member == null) {
             return "home";
