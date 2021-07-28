18486ddb0c1d3f768b3c4ddf4628ed39aaba1bc6
irostub <irostub@gmail.com>
2021-07-28 22:11:22 +0900

    Remove useless importing lib

--- exception/src/main/java/com/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/com/hello/exception/servlet/ErrorPageController.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 @Slf4j
 @Controller
 public class ErrorPageController {
