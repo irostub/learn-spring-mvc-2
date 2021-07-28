195623854d47a20accca75e2e37bac325af41a0a
irostub <irostub@gmail.com>
2021-07-28 22:09:41 +0900

    Add error page view and controller

---
+++ exception/src/main/java/com/hello/exception/servlet/ErrorPageController.java
@@ -0,0 +1,30 @@
+package com.hello.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Controller
+public class ErrorPageController {
+    @RequestMapping("/error-page/404")
+    public String errorPage404() {
+        log.info("errorPage 404");
+        return "error-page/404";
+    }
+
+    @RequestMapping("/error-page/500")
+    public String errorPage500() {
+        log.info("errorPage 500");
+        return "error-page/500";
+    }
+
+    @RequestMapping("/error-page/ex")
+    public String errorPageEx() {
+        log.info("errorPage ex");
+        return "error-page/ex";
+    }
+}
