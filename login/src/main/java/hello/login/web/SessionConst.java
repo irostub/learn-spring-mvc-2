fde4ec3626b44d4b82a02eb33d71e2b0a147eda5
irostub <irostub@gmail.com>
2021-07-28 20:11:46 +0900

    Add @Login annotation
    use custom argument resolver instead of @SessionAttribute in login

--- login/src/main/java/hello/login/web/SessionConst.java
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -1,5 +1,5 @@
 package hello.login.web;
 
-public class SessionConst {
-    public static final String LOGIN_MEMBER = "loginMember";
+public interface SessionConst {
+    String LOGIN_MEMBER = "loginMember";
 }
