ec96ff195802c4ac59b456312909459557327be5
irostub <irostub@gmail.com>
2021-07-29 22:39:32 +0900

    Add UserException and handled as Spring ExceptionHandler

--- exception/src/main/java/com/hello/exception/WebConfig.java
+++ exception/src/main/java/com/hello/exception/WebConfig.java
@@ -2,14 +2,18 @@ package com.hello.exception;
 
 import com.hello.exception.filter.LogFilter;
 import com.hello.exception.interceptor.LogInterceptor;
+import com.hello.exception.resolver.MyHandlerExceptionResolver;
+import com.hello.exception.resolver.UserHandlerExceptionResolver;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -30,5 +34,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/css/**", "/*.ico", "/error", "/error-page/**");
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 }
