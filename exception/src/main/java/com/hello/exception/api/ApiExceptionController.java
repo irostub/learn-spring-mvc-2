ce4430683a0b556b164257b119aacd1e268083da
irostub <irostub@gmail.com>
2021-07-29 23:11:16 +0900

    Add using ResponseStatusException case

--- exception/src/main/java/com/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/com/hello/exception/api/ApiExceptionController.java
@@ -5,10 +5,12 @@ import com.hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
 @RestController
@@ -31,6 +33,11 @@ public class ApiExceptionController {
     @GetMapping("response-status-ex1")
     public String responseStatusEx1() {
         throw new BadRequestException();
+    }
+
+    @GetMapping("response-status-ex2")
+    public String responseStatusEx2() {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
     @Data
