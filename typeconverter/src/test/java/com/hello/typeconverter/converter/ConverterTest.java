139fcdb2bf2b59326f7a5487515e8c26163f1547
irostub <irostub@gmail.com>
2021-08-01 02:39:09 +0900

    Add IpPort convert test

--- typeconverter/src/test/java/com/hello/typeconverter/converter/ConverterTest.java
+++ typeconverter/src/test/java/com/hello/typeconverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package com.hello.typeconverter.converter;
 
+import com.hello.typeconverter.type.IpPort;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +22,21 @@ public class ConverterTest {
         IntegerToStringConverter converter = new IntegerToStringConverter();
         String convertResult = converter.convert(12);
         assertThat(convertResult).isEqualTo("12");
+    }
+
+    @Test
+    @DisplayName("String to IpPort")
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort convert = converter.convert("127.0.0.1:8080");
+        assertThat(convert).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    @DisplayName("IpPort to String")
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        String convert = converter.convert(new IpPort("127.0.0.1", 8080));
+        assertThat(convert).isEqualTo("127.0.0.1:8080");
     }
 }
