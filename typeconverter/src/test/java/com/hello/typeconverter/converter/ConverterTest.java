a4566ed1340b695d1c7537e2a349217b7afc29b8
irostub <irostub@gmail.com>
2021-08-01 02:14:01 +0900

    Add convert test

---
+++ typeconverter/src/test/java/com/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,27 @@
+package com.hello.typeconverter.converter;
+
+import com.hello.typeconverter.controller.IntegerToStringConverter;
+import com.hello.typeconverter.controller.StringToIntegerConverter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConverterTest {
+
+    @Test
+    @DisplayName("String to Integer")
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer convertResult = converter.convert("12");
+        assertThat(convertResult).isEqualTo(12);
+    }
+
+    @Test
+    @DisplayName("Integer to String")
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String convertResult = converter.convert(12);
+        assertThat(convertResult).isEqualTo("12");
+    }
+}
