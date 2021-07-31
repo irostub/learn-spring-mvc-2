55fbbaefc8931d3bbf6fa48328e2dbdcf27cc56f
irostub <irostub@gmail.com>
2021-08-01 00:08:07 +0900

    Add v2 exception controller, use @ExceptionHandler instead of custom exception controller

---
+++ exception/src/main/java/com/hello/exception/exhandler/ErrorResult.java
@@ -0,0 +1,11 @@
+package com.hello.exception.exhandler;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResult {
+    private String code;
+    private String message;
+}
