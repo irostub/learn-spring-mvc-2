1dba175b9d3d579cdcb83fec34b45094171dfe27
irostub <irostub@gmail.com>
2021-08-01 02:35:32 +0900

    Add IpPort domain and converter

---
+++ typeconverter/src/main/java/com/hello/typeconverter/converter/StringToIpPortConverter.java
@@ -0,0 +1,15 @@
+package com.hello.typeconverter.converter;
+
+import com.hello.typeconverter.type.IpPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+@Slf4j
+public class StringToIpPortConverter implements Converter<String, IpPort> {
+    @Override
+    public IpPort convert(String source) {
+        log.info("convert source={}", source);
+        String[] split = source.split(":");
+        return new IpPort(split[0], Integer.parseInt(split[1]));
+    }
+}
