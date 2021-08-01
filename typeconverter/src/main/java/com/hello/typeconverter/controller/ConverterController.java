26fd06bf8ab3aff3418ef2cc18e1db00bd8f6a10
irostub <irostub@gmail.com>
2021-08-01 17:01:55 +0900

    Use type converter with thymeleaf

---
+++ typeconverter/src/main/java/com/hello/typeconverter/controller/ConverterController.java
@@ -0,0 +1,19 @@
+package com.hello.typeconverter.controller;
+
+import com.hello.typeconverter.type.IpPort;
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConverterController {
+
+    @GetMapping("converter-view")
+    public String converterView(Model model) {
+        model.addAttribute("number", 10000);
+        model.addAttribute("ipPort", new IpPort("127.0.0.1", 8080));
+        return "converter-view";
+    }
+
+}
