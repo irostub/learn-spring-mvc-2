5fd64f05d9f58663726ecb16b32143fe5ba170d4
irostub <irostub@gmail.com>
2021-08-01 16:53:46 +0900

    Use type converter with spring

---
+++ typeconverter/src/main/java/com/hello/typeconverter/WebConfigurer.java
@@ -0,0 +1,20 @@
+package com.hello.typeconverter;
+
+import com.hello.typeconverter.converter.IntegerToStringConverter;
+import com.hello.typeconverter.converter.IpPortToStringConverter;
+import com.hello.typeconverter.converter.StringToIntegerConverter;
+import com.hello.typeconverter.converter.StringToIpPortConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
+    }
+}
